fdac04a1f9009436323ca06b8887310feff15641
ruannysil <ruannysilva67@gmail.com>
2025-09-19 16:45:19 -0300

    add: foot redireaction user and developer

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,9 @@ import Hero from "@/components/Hero";
 import IconsSection from "@/components/IconsSection";
 import PlansSection from "@/components/PlansSection";
 import TestimonialCard from "@/components/TestimonialCard";
+import { Button } from "@/components/ui/button";
 import VideoSection from "@/components/VideoSection";
+import Link from "next/link";
 import { useRef, useState } from "react";
 
 export default function Home() {
@@ -21,6 +23,25 @@ export default function Home() {
 
   const scrollToPlans = () => {
     plansRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const handleDeveloper = () => {
+    const whatsappNumber = "5598987312479";
+    const whatsappMessage = `
+Olá! 👋 Sou da Noleto Tech.  
+
+Estou interessado em desenvolver um projeto com vocês e gostaria de mais informações.  
+Tenho interesse em soluções como:  
+- Desenvolvimento de sites  
+- Aplicativos mobile  
+- Sistemas SaaS  
+- Projetos sob medida  
+
+Podemos conversar?
+`.trim();
+
+    const whatsappUrl = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(whatsappMessage)}`;
+    window.open(whatsappUrl, "_blank");
   };
 
   return (
@@ -40,6 +61,11 @@ export default function Home() {
         onClose={() => setIsFormOpen(false)}
         selectedPlan={selectedPlan}
       />
+      <footer className="text-black w-full items-center justify-center p-5 flex gap-1">
+        <p>&copy; todos direito reservados</p>
+        <Button onClick={handleDeveloper} className="bg-transparent hover:bg-transparent text-black cursor-pointer">
+          <strong> Noleto tech</strong></Button>
+      </footer>
     </div>
   );
 }
